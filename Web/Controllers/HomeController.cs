43b2a37e7d308826578d6bb4198a55a17c964b28
MaLiang <maliangnet@126.com>
2013-08-28 14:32:58 +0800

    创建测试项目

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult JS()
+        {
+            return View();
+        }
+
+    }
+}
