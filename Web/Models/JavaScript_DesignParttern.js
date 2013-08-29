c4555c7fcbfa3af1d2a3bcb91d463555cef8c9ec
MaLiang <maliangnet@126.com>
2013-08-29 21:47:36 +0800

    js测试

--- Web/Models/JavaScript_DesignParttern.js
+++ Web/Models/JavaScript_DesignParttern.js
@@ -1,83 +1,26 @@
 ﻿//http://www.cnblogs.com/TomXu/archive/2011/12/15/2288411.html
 //http://www.alloyteam.com/2012/10/common-javascript-design-patterns/
-
+//开始 http://www.cnblogs.com/TomXu/archive/2012/01/05/2305453.html
 //JS基本测试
 (function()
 {
-   //var calculator = function(eq)
-   //    {
-   //         var eqCtl = document.getElementById(eq);
-   //         return{
-   //                     add : function(x,y)
-   //                     {
-   //                         var  val = x + y;
-   //                         eqCtl.innerHTML = val;
-   //                     }
-   //                 };
-   //    }("div").add(5,5);
-    //var calc = new calculator("div");
-    //calculator("div").add(1,2);
-    //calc.add(10,20);
-});
-
-(function()
-{
-    var blogModel = (function(my)
-    {
-        var my = {}, privateName = "博客园";
-        
-        function privateAddTopic(data){return data;};
-       
-        my.name = privateName;
-        my.addTopic = function(data){ return privateAddTopic(data);}
-
-        return my;
-
-    }(blogModel || {}));
-
-    //私有属性多个文件共享
-    (function(my)
-    {
-        var _private = my.private = my.private||{},
-            _sales = my.sales = my.sales || function()
-            {
-                delete my.private;
-                delete my.sales;
-            },
-            _unsale = my.unsale = my.unsale || function()
-            {
-                my.private = _private;
-                my.sales = _sales;
-            };
-    }(blogModel||{}));
+    //var a = document.getElementsByTagName("a");
+    //for(var i=0,max = a.length;i<max;i++)
+    //{
+    //    (function(i)
+    //    {
+    //        a[i].addEventListener("click", function (e)
+    //        {           
+    //            e.preventDefault();
+    //            console.log("click " + i);
+            
+    //        }, false);
+    //    }(i));
+    //}
     
-    console.log(blogModel.name);
-
-    var blogModel = blogModel || {};
-    (function(old)
-    {
-        var my ={},key;
-
-        for(key in old)
-            if(old.hasOwnProperty(key))
-                my[key] = old[key];
-
-        var oldAddPhoto = old.addPhoto;
-        old.addPhoto = function()
-        {
-            console.log("添加addPhoto方法");
-        };
-    }(blogModel || {}));
-
-    console.log(blogModel.addTopic(1));
-
-    blogModel.addPhoto();
-
-}());
-
-(function()
-{
-    //console.log(a);
+    //function foo() { console.log("foo"); foo();};
+    var foo = function () { console.log("foo"); arguments.callee(); }
+    foo();
 }());
 
 //设计模式
