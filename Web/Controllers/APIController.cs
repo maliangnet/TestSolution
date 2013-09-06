53f367e5cb83116fc904fde61b79853d40907a6e
MaLiang <maliangnet@126.com>
2013-09-06 18:05:34 +0800

    提交

---
+++ Web/Controllers/APIController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class API1Controller : Controller
+    {
+        
+        public ActionResult NOCache()
+        {
+            //Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Response.Cache.SetExpires(DateTime.Now.AddSeconds(5));
+            return Content("Ajax");
+        }
+
+        public int Method()
+        {
+            return 1;
+        }
+    }
+}
---
+++ Web/Models/MVC.js
@@ -0,0 +1,18 @@
+﻿(function(jquery)
+{
+
+    $("input[name=ajax]").click(function()
+    {
+        $.ajax(
+            {
+                url : "/API1/NOCache",
+                method :"Get",
+                cache : true,
+                success : function(data)
+                {
+                    //console.log(data);
+                }
+            });
+    });
+
+})();
